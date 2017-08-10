3264cc63d98283d51b5f34b26f8a92083414be4f
arthur.zou <arthur.zou@imaginato.com>
2017-08-10 10:31:47 +0700

    Update

--- src/server/routes/api/index.js
+++ src/server/routes/api/index.js
@@ -1,15 +1,11 @@
 import Router from 'koa-router'
 import ping from './ping'
-import authentication from './authentication'
-import customer from './customer'
 
 const apiRouter = Router({ prefix: '/api' })
 const compose = (...methods) => argument => methods.forEach(method => method.call(this, argument))
 
 compose(
   ping,
-  authentication,
-  customer
 )(apiRouter)
 
 export default apiRouter
